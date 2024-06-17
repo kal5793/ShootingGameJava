9c37d03cc6f0e5b61043dc77dda28f44d0603ff7
Hikaru <zeal23310178@fsgcl.jp>
2024-06-17 11:44:36 +0900

    練習問題10-1のコミット

--- Shooting/src/PlayerBullet.java
+++ Shooting/src/PlayerBullet.java
@@ -4,6 +4,11 @@ public class PlayerBullet extends Character{
 	public PlayerBullet(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
 		// TODO 自動生成されたコンストラクター・スタブ
+		
+	}
+	public void draw(MyFrame f) {
+		f.setColor(0,0,128);
+		f.fillRect(x+10, y,10, 10);
 	}
 
 }
