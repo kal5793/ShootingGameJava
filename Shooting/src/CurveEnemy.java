21d123970fc6df9eaff4405e9da7618912a5effd
Hikaru <zeal23310178@fsgcl.jp>
2024-07-09 10:21:13 +0900

    練習問題13のコミット

--- Shooting/src/CurveEnemy.java
+++ Shooting/src/CurveEnemy.java
@@ -2,7 +2,7 @@
 public class CurveEnemy extends Enemy{
 	public CurveEnemy(double x, double y, double vx, double vy) {
 		super(x,y,vx,vy);
-		life=3;
+		life=3+GameWorld.stage;
 	}
 	public void move() {
 		super.move();
